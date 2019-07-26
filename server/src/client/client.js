ab50219d12b0949145a2895276fc599152e74cf1
ilavisharma <lavisharma796@gmail.com>
2019-07-26 15:19:04 +0530

    client side react setup

---
+++ server/src/client/client.js
@@ -0,0 +1,8 @@
+// Startup point for the client side app
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
