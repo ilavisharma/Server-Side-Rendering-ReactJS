4f2ae2ce2c37a392aae651ce0077ded5b13b559d
ilavisharma <lavisharma796@gmail.com>
2019-07-27 13:18:34 +0530

    store creation added import babel-polyfill

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import 'babel-polyfill';
 import renderer from './Helpers/renderer';
+import createStore from './Helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+  // initilise and load data into the store
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
